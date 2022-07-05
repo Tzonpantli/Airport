c2d338e5d9ffc5d510f5524413960838e303bacb
Tzonpantli <86860824+Tzonpantli@users.noreply.github.com>
2022-07-04 19:38:57 -0500

    Add files via upload

--- spring-security-jwt/src/main/java/com/example/springboot/app/model/Airports.java
+++ spring-security-jwt/src/main/java/com/example/springboot/app/model/Airports.java
@@ -12,5 +12,19 @@ public class Airports {
     @Column(name = "name_airport")
     private  String nameAirport;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNameAirport() {
+        return nameAirport;
+    }
+
+    public void setNameAirport(String nameAirport) {
+        this.nameAirport = nameAirport;
+    }
 }
